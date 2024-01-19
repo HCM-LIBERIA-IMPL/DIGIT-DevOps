cf7f89246cb039cd0cd8db7adcf9996302a5b695
Prasanna S R <prasanna@beehyv.com>
2024-01-19 09:57:48 +0530

    Azure installation startup scripts

---
+++ infra-as-code/terraform/azure-liberia-uat/remote-state/variables.tf
@@ -0,0 +1,15 @@
+variable "tfstate" {
+    default = "demo-azure-container"
+}
+
+variable "environment" {
+    default = "demo-azure-terraform"
+}
+
+variable "location" {
+    default = "South Africa North"
+}
+
+variable "resource_group" {
+    default = "demo-azure-rg-terraform"
+}
